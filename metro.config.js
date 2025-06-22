3020d9f6e578e180a13b7d46fefbe4b1c589e756
GREED & GROSS Developer <whitecasteddu@gmail.com>
2025-06-22 13:19:16 +0200

    Aggiungi metro.config.js - ERRORE: No Metro config found

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+/**
+ * Metro configuration for React Native
+ * https://reactnative.dev/docs/metro
+ *
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
