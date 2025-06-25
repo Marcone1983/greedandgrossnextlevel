f111aed9b5e04f7a514da0993fa5630570e933ce
GREED & GROSS Developer <whitecasteddu@gmail.com>
2025-06-25 15:33:14 +0200

    Fix autolinking configuration in react-native.config.js
    - Add buildGradlePath and appName to android config
    - Add dependencies section to help with autolinking
    - Set environment variables for React Native paths

--- react-native.config.js
+++ react-native.config.js
@@ -5,6 +5,18 @@ module.exports = {
       sourceDir: './GreedGross',
       manifestPath: 'app/src/main/AndroidManifest.xml',
       packageName: 'com.greedandgross.cannabisbreeding',
+      buildGradlePath: 'build.gradle',
+      appName: 'app',
+    },
+  },
+  dependencies: {
+    // Ensure all native modules are autolinked
+    'react-native-reanimated': {
+      platforms: {
+        android: {
+          sourceDir: '../node_modules/react-native-reanimated/android',
+        },
+      },
     },
   },
 };
